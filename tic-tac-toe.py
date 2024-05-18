7949d32082ee97495eb851507065073aa61fba24
akhi07rx <89210430+akhi07rx@users.noreply.github.com>
2024-05-18 22:59:05 +0530

    added a sample layout

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -10,10 +10,35 @@ class TicTacToeBoard(tk.Tk):
         super().__init__()
         self.title("TIC TAC TOE")
         self.cells = {}
+        self.create_board_display()
+        self.create_board_grid()
 
     def create_board_display(self):
         display_frame = tk.Frame(master=self)
         display_frame.pack(fill=tk.X)
+
         self.display = tk.Label(
-            master=display_frame, text="READY?", font=font.Font(size=28, weight="bold"),)
+            master=display_frame, text="READY?", font=font.Font(size=28, weight="bold"),
+        )
         self.display.pack()
+
+    def create_board_grid(self):
+        grid_frame = tk.Frame(master=self)
+        grid_frame.pack()
+        for row in range(3):
+            self.rowconfigure(row, weight=1, minsize=50)
+            self.columnconfigure(row, weight=1, minsize=75)
+            for col in range(3):
+                button = tk.Button(master=grid_frame, text="",
+                                   font=font.Font(size=36, weight="bold"), fg="Black", width=3, height=2, highlightbackground="lightblue",)
+                self.cells[button] = (row, col)
+                button.grid(row=row, column=col, padx=5, pady=5, sticky="nsew")
+
+
+def main():
+    board = TicTacToeBoard()
+    board.mainloop()
+
+
+if __name__ == "__main__":
+    main()
